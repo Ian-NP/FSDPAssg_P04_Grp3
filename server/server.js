b989b8651c361c6d04697f1ae9e7d2fcecbc6767
Ian <iancheif117@gmail.com>
2024-11-03 14:56:54 +0800

    Merging?

--- server/server.js
+++ server/server.js
@@ -17,11 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", transactionRoutes)
 
-<<<<<<< HEAD
-//app.get("/api", (req, res) =>{
-    //res.json({"fruits": ["apple", "banana", "orange"]});
-//})
-=======
 app.get("/api/Accounts", accountController.getAllAccounts);
 app.get("/api/Accounts/firebaseid/:id", accountController.getAccountById);
 app.get("/api/Accounts/:accountNum", accountController.getAccountByAccountNum);
@@ -29,7 +24,6 @@ app.post("/api/Accounts/login", accountController.loginAccount);
 app.post("/api/Accounts/create", accountController.createAccount);
 app.put("/api/Accounts/updateBalance/:accountNum", accountController.updateBalance);
 app.delete("/api/Accounts/remove/:id", accountController.deleteAccount);
->>>>>>> 0acf7dd04ca5b53e38f374f418fa1a9ca47467e9
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
