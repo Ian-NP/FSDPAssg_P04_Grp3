4aa0a09ace9d4d76ea4763f45666712c8b2f668a
Ian <iancheif117@gmail.com>
2024-11-14 18:16:48 +0800

    Helped Edison fixed his stuff

--- client/src/pages/PreOrderWithdrawalConfirmation.jsx
+++ client/src/pages/PreOrderWithdrawalConfirmation.jsx
@@ -21,38 +21,40 @@ const PreOrderWithdrawalConfirmation = () => {
         }
     }, [accountDetails, amount]);
 
-    const handleContinue = async () => {
-        // Define transaction data to send to the backend
-        const transactionData = {
-            accountId: accountDetails?.id, // Ensure account ID is available in accountDetails
-            amount: parseInt(amount),
-            transactionType: "withdrawal"
-        };
+    // *IMPORTANT*
+    // IAN HERE, I COMMENTED THIS PART OUT BECAUSE THIS SHOULD BE HANDLED BY AFTER SCANNING THE QR CODE AT THE ATM
+    // const handleContinue = async () => {
+    //     // Define transaction data to send to the backend
+    //     const transactionData = {
+    //         accountId: accountDetails?.id, // Ensure account ID is available in accountDetails
+    //         amount: parseInt(amount),
+    //         transactionType: "withdrawal"
+    //     };
 
-        // Make POST request to log the transaction
-        try {
-            const response = await axios.post('http://localhost:3000/api/transactions', transactionData);
+    //     // Make POST request to log the transaction
+    //     try {
+    //         const response = await axios.post('http://localhost:3000/api/transactions', transactionData);
 
-            if (response.data.success) {
-                console.log("Transaction created successfully:", response.data);
-                setSuccess(true);
+    //         if (response.data.success) {
+    //             console.log("Transaction created successfully:", response.data);
+    //             setSuccess(true);
 
-                // Navigate to the ReceiptChoice page with transaction details
-                navigate("/receiptChoice", { 
-                    state: { 
-                        transactionType: "withdrawal", 
-                        amount: parseInt(amount), 
-                        email: accountDetails.email 
-                    } 
-                });
-            } else {
-                alert("An error occurred while recording the transaction. Please try again.");
-            }
-        } catch (error) {
-            console.error("Error creating transaction:", error);
-            alert("An error occurred while recording the transaction. Please try again.");
-        }
-    };
+    //             // Navigate to the ReceiptChoice page with transaction details
+    //             navigate("/receiptChoice", { 
+    //                 state: { 
+    //                     transactionType: "withdrawal", 
+    //                     amount: parseInt(amount), 
+    //                     email: accountDetails.email 
+    //                 } 
+    //             });
+    //         } else {
+    //             alert("An error occurred while recording the transaction. Please try again.");
+    //         }
+    //     } catch (error) {
+    //         console.error("Error creating transaction:", error);
+    //         alert("An error occurred while recording the transaction. Please try again.");
+    //     }
+    // };
 
     const handleSlideComplete = () => {
         console.log('QR Code Generated');
@@ -61,6 +63,7 @@ const PreOrderWithdrawalConfirmation = () => {
 
     return (
         <Layout>
+            <p>{amount}</p>
             <div className={styles.container}>
                 <section className={styles.infoSection}>
                     <div className={styles.row}>
--- client/src/pages/PreOrderWithdrawalScreen.jsx
+++ client/src/pages/PreOrderWithdrawalScreen.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate, useLocation  } from 'react-router-dom'; 
 import Header from '../components/Header';
 import Button from '../components/Button';
 import commonStyles from "../styles/Common.module.css"; 
 import styles from '../styles/Withdrawal.module.css';
-import { useAccount } from '../contexts/AccountContext';
-import { useATM } from '../contexts/AtmContext';
 import axios from 'axios';
 import OutOfCash from './OutOfCash'; // Import OutOfCash modal component
 
 const PreOrderWithdrawalScreen = () => {
   const [amount, setAmount] = useState("0.00");
+  // Access the state passed via navigation
+  const { state } = useLocation();
+  const { accountDetails } = state || {}; // Destructure the state safely
   const [showOutOfCashModal, setShowOutOfCashModal] = useState(false); // State to control modal visibility
-  const { accountDetails, withdrawFromAccount } = useAccount();
-  const { cashLevels, withdrawCash } = useATM();
   const navigate = useNavigate(); 
 
   const baseURL = `${window.location.protocol}//${window.location.hostname}:3000`;
@@ -23,83 +22,21 @@ const PreOrderWithdrawalScreen = () => {
     setAmount("0.00");
   };
 
+  console.log(accountDetails);
+  const accountBalance = accountDetails?.balance;
+  const accountName = accountDetails?.account_name;
+  const accountNum = accountDetails?.account_num;
+
   const handleConfirm = async () => {
-    console.log("Confirm button clicked with amount:", amount);
-  
-    // Validate the amount
     if (amount % 10 !== 0 && amount % 50 !== 0) {
       alert("Please enter amount in multiples of $10 or $50");
       return;
-    } else if (amount > accountDetails.balance) {
+    } else if (amount > accountBalance) {
       alert("Insufficient balance. Please enter a valid amount.");
       return;
     } else {
       console.log("Amount is valid");
-  
-      // Calculate the number of $50 and $10 notes required
-      let remainingAmount = parseInt(amount);
-      const required50Notes = Math.floor(remainingAmount / 50);
-      remainingAmount -= required50Notes * 50;
-      const required10Notes = remainingAmount / 10;
-  
-      // Check if the ATM has enough $50 and $10 notes
-      if (cashLevels[50] >= required50Notes && cashLevels[10] >= required10Notes) {
-        // Proceed with the withdrawal
-        const success = await withdrawFromAccount(amount); // Wait for the withdrawal to complete
-  
-        if (success) {
-          // Prepare transaction data
-          const transactionData = {
-            amount: parseInt(amount),
-            description: "",
-            destination_account: null,
-            source_account_id: accountDetails.account_num,
-            status: "Completed",
-            transaction_date: Date.now(), // Current datetime in milliseconds
-            transaction_type: "withdrawal",
-            cateogry: "cash",
-          };
-  
-          // Make POST request to log the transaction
-          try {
-            const response = await axios.post(`${baseURL}/api/transactions`, transactionData);
-  
-            if (response.data.success) {
-              console.log("Transaction created successfully:", response.data);
-  
-              // After successful transaction, update the ATM cash level
-              const atmUpdateResponse = withdrawCash(parseInt(amount));
-  
-              if (atmUpdateResponse) {
-                console.log("ATM cash level updated successfully");
-  
-                // Passing the data to PreOrderQR screen
-                navigate("/PreOrderQR", {
-                    state: {
-                    amount: parseInt(amount),
-                    accountDetails: {
-                        accountName: accountDetails.account_name,
-                        accountNum: accountDetails.account_num
-                    }
-                    }
-                });
-              } else {
-                alert("An error occurred while updating the ATM cash level. Please try again.");
-              }
-            } else {
-              alert("An error occurred while recording the transaction. Please try again.");
-            }
-          } catch (error) {
-            console.error("Error creating transaction:", error);
-            alert("An error occurred while recording the transaction. Please try again.");
-          }
-        } else {
-          alert("An error occurred during the withdrawal. Please try again."); // Error handling
-        }
-      } else {
-        // Show the out of cash modal if not enough notes are available
-        setShowOutOfCashModal(true); // Show the modal
-      }
+      navigate('/preOrderWithdrawalConfirmation', { state: { amount, accountDetails } });
     }
   };
 
